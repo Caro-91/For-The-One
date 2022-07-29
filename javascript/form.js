d3e3a86ca82628c9365e6b1be58f11c59fbf5b2f
Caroline Gonzales <worshippercaroline@gmail.com>
2022-07-28 22:41:53 -0400

    Add form validation and style

---
+++ javascript/form.js
@@ -0,0 +1,85 @@
+
+const emailEl = document.querySelector('#email');
+const form = document.querySelector('#signup');
+
+const checkEmail = () => {
+    let valid = false;
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+        showError(emailEl, 'Email cannot be blank.');
+    } else if (!isEmailValid(email)) {
+        showError(emailEl, 'Email is not valid.')
+    } else {
+        showSuccess(emailEl);
+        valid = true;
+    }
+    return valid;
+};
+
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
+const isRequired = value => value === '' ? false : true;
+
+const showError = (input, message) => {
+    // get the form-field element
+    const formField = input.parentElement;
+    // add the error class
+    formField.classList.remove('success');
+    formField.classList.add('error');
+
+    // show the error message
+    const error = formField.querySelector('small');
+    error.textContent = message;
+};
+
+const showSuccess = (input) => {
+    // get the form-field element
+    const formField = input.parentElement;
+
+    // remove the error class
+    formField.classList.remove('error');
+    formField.classList.add('success');
+
+    // hide the error message
+    const error = formField.querySelector('small');
+    error.textContent = '';
+}
+
+
+form.addEventListener('submit', function (e) {
+    // prevent the form from submitting
+    e.preventDefault();
+
+    // validate fields
+    let isEmailValid = checkEmail();
+
+    // submit to the server if the form is valid
+    if (isEmailValid) {
+        const email = emailEl.value.trim();
+        localStorage.setItem("email", email);
+    }
+});
+
+
+const debounce = (fn, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+        // cancel the previous timer
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        // setup a new timer
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args)
+        }, delay);
+    };
+};
+
+form.addEventListener('input', debounce(function (e) {
+    if(e.target.id === "email") {
+        checkEmail();
+    }
+}));
---
+++ javascript/script.js
@@ -0,0 +1,48 @@
+
+  window.addEventListener("DOMContentLoaded",function(e){
+   console.log('working')
+   const orderButtons = document.querySelectorAll("button[data-order]"); 
+
+   orderButtons.forEach (function(button) {
+
+     button.addEventListener("click", function(e) {
+       const button = e.currentTarget;
+       const container = button.parentNode;
+
+       console.log('container', container.querySelector(".title"))
+       const order = {
+         id : button.getAttribute("data-order"),
+         title : container.querySelector(".title").innerText,
+         price : container.querySelector(".price").innerText,
+         desc : container.querySelector(".desc").innerText,
+       };
+       localStorage.setItem("order", JSON.stringify(order));
+       const url = window.location.href.replace("flowerpage.html","cart.html");
+         window.location.href = url;
+       });
+     });
+   });
+   window.addEventListener("DOMContentLoaded",function(e){
+      const order = localStorage.getItem ("order");
+      if (order){
+          const flowerOrder = JSON.parse(order);
+
+          const orderInput = document.querySelector("#flower-order");
+          orderInput.value = order;
+
+          const flower = document.querySelector(".flower");
+
+          const title = flower.querySelector(".title");
+          const price = flower.querySelector(".price");
+          const desc = flower.querySelector(".desc");
+
+          title.innerText = flowerOrder.title;
+          price.innerText = flowerOrder.price;
+          desc.innerText = flowerOrder.desc;
+           
+          const img = flower.querySelector("img");
+          img.setAttribute("scr",'images/${flowerOrder.id}.jpg');
+          img.setAttribute("alt",flowerOrder.title);
+      }
+
+  });
